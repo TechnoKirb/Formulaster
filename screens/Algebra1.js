e3b3406bacf0af9fc6d1642eec30e964d39d80a6
TechnoKirb <87034229+TechnoKirb@users.noreply.github.com>
2023-08-17 23:26:29 -0700

    Update Algebra1.js

--- screens/Algebra1.js
+++ screens/Algebra1.js
@@ -72,7 +72,8 @@ export default class Algebra1 extends Component {
                 borderColor: 'white',
               }}
               textStyle={{
-                color: this.state.dropdownHeight == 170 ? 'black' : 'white'
+                color: this.state.dropdownHeight == 170 ? 'black' : 'white',
+                fontFamily: 'Bubblegum-Sans',
               }}
               onSelectItem={(item) =>
                 this.setState({
@@ -114,6 +115,7 @@ const styles = StyleSheet.create({
   appTitleText: {
     color: 'white',
     fontSize: RFValue(28),
+    fontFamily: 'Bubblegum-Sans',
     marginLeft: 100,
   },
   fieldsContainer: {
